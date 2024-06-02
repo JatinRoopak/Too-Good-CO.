8c4fdac79e6f0a2dcc2a8f7dcc49739b4d4b5c16
JatinRoopak <jatin.roopak.xb.8195@gmail.com>
2024-06-02 18:40:19 +0530

    Until page 1

--- toogood.js
+++ toogood.js
@@ -1,36 +1,43 @@
-function locomotiveanimation() {
+function locomotiveAnimation() {
     gsap.registerPlugin(ScrollTrigger);
 
     // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
     const locoScroll = new LocomotiveScroll({
         el: document.querySelector("#main"),
-        smooth: true
+        smooth: true,
     });
     // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
     locoScroll.on("scroll", ScrollTrigger.update);
 
-    // tell ScrollTrigger to use these proxy methods for the "#mai" element since Locomotive Scroll is hijacking things
-    ScrollTrigger.scrollerProxy("#mai", {
+    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+    ScrollTrigger.scrollerProxy("#main", {
         scrollTop(value) {
-            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+            return arguments.length
+                ? locoScroll.scrollTo(value, 0, 0)
+                : locoScroll.scroll.instance.scroll.y;
         }, // we don't have to define a scrollLeft because we're only scrolling vertically.
         getBoundingClientRect() {
-            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+            return {
+                top: 0,
+                left: 0,
+                width: window.innerWidth,
+                height: window.innerHeight,
+            };
         },
         // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-        pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+        pinType: document.querySelector("#main").style.transform
+            ? "transform"
+            : "fixed",
     });
 
-
-    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
     ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
     // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
     ScrollTrigger.refresh();
-
 }
-locomotiveanimation()
+locomotiveAnimation();
 
 function landingtitle() {
     var tl = gsap.timeline()
@@ -52,81 +59,99 @@ function landingtitle() {
 }
 landingtitle()
 
-function menuanimation() {
-    document.querySelector("#navbar-icons a").addEventListener("click", function (event) {
-        event.preventDefault();
-    })
-    let ismenuclose = true
-    document.getElementById("menuicon").addEventListener("click", function () {
-        var menu = document.querySelector("#menu")
-        var links = document.querySelectorAll("#nav-part-2 a")
-        var logo = document.querySelector("#logo svg")
-        var backgroundicons = document.querySelector('#navbar-icons')
+function navbarAnimations(){
 
-        var menulinks = document.querySelectorAll(".menulinks h2")
-        var elem = document.querySelector("#elem404")
+    function scrolltriggers(){
+        gsap.to("#nav-part-2 .links", {
+            opacity: 0,
+            scrollTrigger: {
+                trigger: "#page1",
+                scroller: "#main",
+                start: "top 0",
+                end: "top -5%",
+                scrub: true,
+            }
+        });
+    }
+    scrolltriggers()
 
-        if (ismenuclose == false) {
-            gsap.to(backgroundicons, {
-                background: "#F7F7F7"
-            })
-            var tback = gsap.timeline()
-
-            tback.to(elem, {
-                opacity: 0,
-                duration: 0.3
-            })
-
-            tback.to(menu, {
-                height: "0vh",
-                duration: 0.3
-            });
-
-            tback.to(links, {
-                color: "black"
-            }, '-=0.3');
-
-            tback.to(logo, {
-                color: "black"
-            }, '-=0.3');
-
-            ismenuclose = true
-        }
-
-        else {
-            gsap.to(backgroundicons, {
-                background: "transparent"
-            })
-
-            gsap.to(menu, {
-                height: "100vh",
-                duration: 0.4
-            });
-            gsap.to(logo, {
-                color: "white"
-            });
-
-            links.forEach(link => {
-                gsap.to(link, {
-                    color: "white"
+    function menuanimation() {
+        document.querySelector("#navbar-icons a").addEventListener("click", function (event) {
+            event.preventDefault();
+        })
+        let ismenuclose = true
+        document.getElementById("menuicon").addEventListener("click", function () {
+            var menu = document.querySelector("#menu")
+            var links = document.querySelectorAll("#nav-part-2 a")
+            var logo = document.querySelector("#logo svg")
+            var backgroundicons = document.querySelector('#navbar-icons')
+    
+            var menulinks = document.querySelectorAll(".menulinks h2")
+            var elem = document.querySelector("#elem404")
+    
+            if (ismenuclose == false) {
+                gsap.to(backgroundicons, {
+                    background: "#F7F7F7"
                 })
-            });
+                var tback = gsap.timeline()
+    
+                tback.to(elem, {
+                    opacity: 0,
+                    duration: 0.3
+                })
+    
+                tback.to(menu, {
+                    height: "0vh",
+                    duration: 0.3
+                });
+    
+                tback.to(links, {
+                    color: "black"
+                }, '-=0.3');
+    
+                tback.to(logo, {
+                    color: "black"
+                }, '-=0.3');
+    
+                ismenuclose = true
+            }
+    
+            else {
+                gsap.to(backgroundicons, {
+                    background: "transparent"
+                })
+    
+                gsap.to(menu, {
+                    height: "100vh",
+                    duration: 0.4
+                });
+                gsap.to(logo, {
+                    color: "white"
+                });
+    
+                links.forEach(link => {
+                    gsap.to(link, {
+                        color: "white"
+                    })
+                });
+    
+                var tl = gsap.timeline()
+                tl.from(menulinks, {
+                    y: "350px",
+                    ease: "power2.out",
+                    duration: 0.55
+                });
+                tl.to(elem, {
+                    opacity: "1",
+                    duration: 0.4
+                })
+    
+                ismenuclose = false
+            }
+        });
+    }
+    menuanimation()
 
-            var tl = gsap.timeline()
-            tl.from(menulinks, {
-                y: "350px",
-                ease: "power2.out",
-                duration: 0.55
-            });
-            tl.to(elem, {
-                opacity: "1",
-                duration: 0.4
-            })
 
-            ismenuclose = false
-        }
-    });
 }
-menuanimation()
-
-
+navbarAnimations()
